f20bfc19b9646d662a4cf23006c601ed392d0ce5
Camilleus <camilleusrex@gmail.com>
2026-04-08 19:04:17 +0200

    add django-import-export for products

--- core/settings.py
+++ core/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'products',
     'sellers',
     'orders',
+    'import_export',
 ]
 
 MIDDLEWARE = [
--- products/admin.py
+++ products/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 from .models import Product
 
+class ProductResource(resources.ModelResource):
+    class Meta:
+        model = Product
+        fields = ('id', 'name', 'brand', 'price', 'gender', 'scent_notes', 'description', 'stock_quantity', 'is_available', 'image')
+
 @admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
+class ProductAdmin(ImportExportModelAdmin):
+    resource_classes = [ProductResource]
     list_display = ['name', 'brand', 'price', 'stock_quantity', 'gender', 'is_available']
     list_filter = ['brand', 'gender', 'is_available']
     search_fields = ['name', 'brand', 'scent_notes']
