c577db1e8f87eee57a2cc204119b31a4f7d92151
Camilleus <camilleusrex@gmail.com>
2026-04-16 21:32:11 +0200

    added more filters and sorting options

--- products/views.py
+++ products/views.py
@@ -2,11 +2,12 @@ from .models import Product
 from django.shortcuts import get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, redirect
+from django.db.models import Q
 
 def product_list(request):
-    products = Product.objects.all()
+    products = Product.objects.filter(is_available=True)
 
-    # Filtrowanie (jeśli masz już – zostaw)
+    # Filtrowanie (istniejące)
     gender = request.GET.get('gender')
     if gender:
         products = products.filter(gender=gender)
@@ -15,20 +16,39 @@ def product_list(request):
     if brand:
         products = products.filter(brand__icontains=brand)
 
-    # Paginacja – 24 produkty na stronę
+    # Nowe: sortowanie
+    sort_by = request.GET.get('sort')
+    if sort_by == 'price_asc':
+        products = products.order_by('price')
+    elif sort_by == 'price_desc':
+        products = products.order_by('-price')
+    elif sort_by == 'name_asc':
+        products = products.order_by('name')
+    elif sort_by == 'name_desc':
+        products = products.order_by('-name')
+    elif sort_by == 'newest':
+        products = products.order_by('-id')  # lub '-created_at' jeśli masz
+    else:
+        products = products.order_by('name')  # domyślnie po nazwie
+
+    # Paginacja
     paginator = Paginator(products, 24)
     page = request.GET.get('page')
     try:
         products_page = paginator.page(page)
-    except PageNotAnInteger:
+    except:
         products_page = paginator.page(1)
-    except EmptyPage:
-        products_page = paginator.page(paginator.num_pages)
+
+    # Pobierz unikalne marki dla filtra
+    brands = Product.objects.filter(is_available=True).values_list('brand', flat=True).distinct().order_by('brand')
 
     return render(request, 'products/list.html', {
-    'products': products_page,   # dla pętli
-    'page_obj': products_page,   # dla paginacji
-    'paginator': paginator,
+        'products': products_page,
+        'page_obj': products_page,
+        'brands': brands,
+        'current_brand': request.GET.get('brand', ''),
+        'current_sort': sort_by,
+        'current_gender': gender,
     })
 
 
