6295fe39c6cc2f6ace29fc0599614fb93967102d
Camilleus <camilleusrex@gmail.com>
2026-04-16 16:21:19 +0200

    added pagination

--- products/urls.py
+++ products/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.product_list, name='product_list'),
     path('produkt/<slug:slug>/', views.product_detail, name='product_detail'),
     path('quiz/', views.quiz, name='quiz'),
+    path('quiz/', views.quiz, name='quiz'),
+    path('quiz/reset/', views.quiz_reset, name='quiz_reset'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,23 +1,34 @@
-from django.shortcuts import render
 from .models import Product
 from django.shortcuts import get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import render, redirect
 
 def product_list(request):
-    products = Product.objects.filter(is_available=True)
-    
+    products = Product.objects.all()
+
+    # Filtrowanie (jeśli masz już – zostaw)
     gender = request.GET.get('gender')
-    brand = request.GET.get('brand')
-    
     if gender:
         products = products.filter(gender=gender)
+
+    brand = request.GET.get('brand')
     if brand:
         products = products.filter(brand__icontains=brand)
-    
-    brands = Product.objects.filter(is_available=True).values_list('brand', flat=True).distinct()
-    
+
+    # Paginacja – 24 produkty na stronę
+    paginator = Paginator(products, 24)
+    page = request.GET.get('page')
+    try:
+        products_page = paginator.page(page)
+    except PageNotAnInteger:
+        products_page = paginator.page(1)
+    except EmptyPage:
+        products_page = paginator.page(paginator.num_pages)
+
     return render(request, 'products/list.html', {
-        'products': products,
-        'brands': brands,
+    'products': products_page,   # dla pętli
+    'page_obj': products_page,   # dla paginacji
+    'paginator': paginator,
     })
 
 
@@ -26,12 +37,22 @@ def product_detail(request, slug):
     return render(request, 'products/detail.html', {'product': product})
 
 def quiz(request):
-    results = None
+    # Jeśli POST – zapisz odpowiedzi w sesji i przekieruj na GET
     if request.method == 'POST':
-        intensity = request.POST.get('intensity')
-        category = request.POST.get('category')
-        occasion = request.POST.get('occasion')
-        gender = request.POST.get('gender')
+        request.session['quiz_intensity'] = request.POST.get('intensity')
+        request.session['quiz_category'] = request.POST.get('category')
+        request.session['quiz_occasion'] = request.POST.get('occasion')
+        request.session['quiz_gender'] = request.POST.get('gender')
+        return redirect('quiz')
+
+    # Dla GET – odczytaj filtry z sesji
+    intensity = request.session.get('quiz_intensity')
+    category = request.session.get('quiz_category')
+    occasion = request.session.get('quiz_occasion')
+    gender = request.session.get('quiz_gender')
+
+    results = None
+    if intensity and occasion:   # wymagane pola
         results = Product.objects.filter(
             is_available=True,
             intensity=intensity,
@@ -41,4 +62,29 @@ def quiz(request):
             results = results.filter(category=category)
         if gender:
             results = results.filter(gender=gender)
-    return render(request, 'products/quiz.html', {'results': results})
+
+    # Paginacja
+    if results:
+        paginator = Paginator(results, 24)
+        page = request.GET.get('page')
+        try:
+            results_page = paginator.page(page)
+        except PageNotAnInteger:
+            results_page = paginator.page(1)
+        except EmptyPage:
+            results_page = paginator.page(paginator.num_pages)
+    else:
+        results_page = None
+
+    return render(request, 'products/quiz.html', {
+        'results': results_page,      # produkty (stronicowane)
+        'page_obj': results_page,     # dla paginacji
+    })
+
+
+def quiz_reset(request):
+    """Wyczyść wszystkie zapisane odpowiedzi quizu w sesji"""
+    for key in list(request.session.keys()):
+        if key.startswith('quiz_'):
+            del request.session[key]
+    return redirect('quiz')
