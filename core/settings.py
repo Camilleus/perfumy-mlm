493fb471b590b21a4f0711b10e567bcd2572034a
Camilleus <camilleusrex@gmail.com>
2026-04-24 13:56:57 +0200

    one fix in default from email

--- core/settings.py
+++ core/settings.py
@@ -104,7 +104,7 @@ LOGOUT_REDIRECT_URL = '/'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = config('EMAIL_HOST', default='')
 EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
-EMAIL_USE_TLS = True
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')       # adres e-mail do wysyłki
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')   # hasło aplikacji lub zwykłe hasło
 DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='') # opcjonalne
